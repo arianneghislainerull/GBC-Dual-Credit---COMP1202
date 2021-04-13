efcde78964a68629dd364b3fe3f099c297dc2c32
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2021-04-13 12:58:42 -0400

    update

---
+++ FoodClassifier/FoodClassifier/Program.cs
@@ -0,0 +1,41 @@
+﻿// define a scope of which an object will be disposed [storage/data structure]
+
+using System;
+using System.Collections;
+
+namespace FoodClassifier // declare a scope
+{
+    class Program // class
+    {
+        static void Main(string[] args) // main method
+        {
+            Hashtable foodSelection = new Hashtable() // create a hashtable
+            {
+                { "Bacon-Pimiento Guacamole", "Appetizer" },
+                { "Crispy Baked Chicken Wings", "Appetizer" },
+                { "Ham-and-Cheese Sliders", "Appetizer" },
+                { "Sweet Potato and Chorizo Sausage Bites", "Appetizer" },
+                { "Quick Pickled Brussels Sprouts", "Appetizer" },
+                { "Mini Black Bean and Cheese Enchiladas", "Appetizer" },
+                { "Cheese Snappy Wafers", "Appetizer"},
+                { "Cardamom Maple Salmon", "Main Course"},
+                { "Spicy Pork Tenderloin with Apples and Sweet Potatoes", "Main Course"},
+                { "Gemelli Pasta with Roasted Pumpkin and Pancetta", "Main Course"},
+                { "Cauliflower Bolognese", "Main Course"},
+                { "Chicken Stew with Potatoes and Radishes", "Main Course"},
+                { "Chocolate Cookie Icebox Dessert", "Dessert"},
+                { "Creamy Rice Pudding", "Dessert"},
+                { "Spanish Flan", "Dessert"},
+                { "Tiramisu", "Dessert"},
+                { "S'mores", "Dessert"},
+                { "Rum Cake", "Dessert"},
+                { "Toasted Coconut Toffee Chocolate Chip Cookies", "Dessert"},
+                { "Malva Pudding", "Dessert"}
+            };
+            // perform operation "for each" object in the hashmap
+            foreach (DictionaryEntry food in foodSelection)
+                // operation to be performed
+                Console.WriteLine("Food's Name: {0} \nFood Type: {1}", food.Key, food.Value, "\n");
+        }
+    }
+}
---
+++ Week6Lesson/Week6Lesson/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Week6Lesson
+{
+    class Program
+    {
+
+        public static void main_menu()
+        {
+            Console.WriteLine("============= Main Menu ==============");
+            Console.WriteLine(" 1 - Calculator (two numbers)");
+            Console.WriteLine(" 2 - Play a game ");
+            Console.WriteLine(" 3 - Exit ");
+            Console.WriteLine("=====================================");
+        }
+
+        public static void sub1_menu()
+        {
+            Console.WriteLine("============= Main Menu ==============");
+            Console.WriteLine(" 1 - Add two numbers");
+            Console.WriteLine(" 2 - Subtract two numbers");
+            Console.WriteLine(" 3 - Multiple two numbers");
+            Console.WriteLine(" 4 - Divide two numbers");
+            Console.WriteLine(" 5 - Return to the menu");
+            Console.WriteLine("=====================================");
+        }
+
+        public static void math_menu()
+        {
+            while (true)
+            {
+                sub1_menu(); // show the main menu
+                Console.WriteLine("Select one of the menu");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                double result = 0; double a = 0; double b = 0;
+                if (select == 1 || select == 2 || select == 3 || select == 4)
+
+                {
+                    Console.WriteLine("Enter two numbers in each line");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    b = Convert.ToDouble(Console.ReadLine());
+                }
+
+                
+
+                // double result = 0;
+
+                if (select == 1)
+                {
+                    //result = a + b;
+                    Console.WriteLine($"The result is { a+b }");
+                }
+                else if (select == 2)
+                {
+                    //result = a - b;
+                    Console.WriteLine($"The result is { a - b}");
+                }
+
+                else if (select == 3)
+                {
+                    //result = a * b;
+                    Console.WriteLine($"The result is { a * b}");
+                }
+
+                else if (select == 4)
+                {
+                    //result = a / b;
+                    Console.WriteLine($"The result is { a / b}");
+                }
+
+                else if (select == 5)
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("T");
+                }
+
+                Console.WriteLine($"The result is {result}");
+            }
+        }
+
+        public static void sub2_menu()
+        {
+            Console.WriteLine("============= Main Menu ==============");
+            Console.WriteLine(" 1 - Single Player");
+            Console.WriteLine(" 2 - Multiplayer");
+            Console.WriteLine(" 3 - Return to the main menu");
+            Console.WriteLine("=====================================");
+        }
+
+        public static void game_menu()
+        {
+            while (true)
+            {
+                //sub2_menu
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+             1) menu driven programming
+             2) input validation and exception handling
+
+             */
+
+            while (true)
+            {
+                main_menu(); // show the main menu
+                Console.WriteLine("Select one of the menu");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                if (select == 1)
+                {
+                    Console.WriteLine("Option 1 is selected");
+                    // math menu
+                    math_menu();
+                }
+                else if (select == 2)
+                {
+                    Console.WriteLine("Option 2 is selected");
+                    // game menu
+                    game_menu();
+                }
+                else if (select == 3)
+                {
+                    Console.WriteLine("Option 3 is selected");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+}
